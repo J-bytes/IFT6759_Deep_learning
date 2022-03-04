0cf2a65b97bd604ef77f28ddf4574bd201b6c784
jonathan <joe199827@gmail.com>
2022-03-04 14:02:01 -0500

    updated cct organizer

--- data_API/data_organizer_CCT.py
+++ data_API/data_organizer_CCT.py
@@ -11,24 +11,25 @@ image_initial_direcory="/mnt/g/cct_images" # full path required
 if not os.path.exists(image_final_directory):
     os.makedirs(image_final_directory)
 new_dict={}
-for image in data["images"] : #gonna be freaking long
+for image in data["images"] : # boucle sur le fichier json ; liste des images
     try :
         location=image["location"]
-        src=f"{image_initial_direcory}/{image['file_name']}"
-        dst=f"{image_final_directory}/{location}/{image['file_name']}"
-        if not os.path.exists(f"{image_final_directory}/{location}") :
+        src=f"{image_initial_direcory}/{image['file_name']}" #fichier source
+        dst=f"{image_final_directory}/{location}/{image['file_name']}" # destinatioon ou copier l'image "data/images/location/image.png
+        if not os.path.exists(f"{image_final_directory}/{location}") : #create folder if location does not exist
             os.makedirs(f"{image_final_directory}/{location}")
 
         shutil.copy(src,dst)
-        #os.remove(src)
-        new_file_dict=image
-        for annotation in data["annotations"] :
-            if annotation["image_id"]==image["id"] :
-                for category in data["categories"] :
+        #os.remove(src) # remove the original to not overload disk space
+        new_file_dict=image # copy the dictionnary with the different PARTIAL info about the image
+        for annotation in data["annotations"] : #boucle sur la liste des annotations
+            if annotation["image_id"]==image["id"] : #trouver l'annotation correspondant à l'image
+                for category in data["categories"] : # trouver la catégorie correspondant à celle dans l'annotation
                     if annotation["category_id"]==category["id"] :
-                        annotation["category"]=category["name"]
-                new_file_dict=new_file_dict|annotation
-        if "category" in new_file_dict :
+                        annotation["category"]=category["name"] # add the category key to the dictionnary annnotation
+                new_file_dict=new_file_dict|annotation # merges the dict new_file_dict and annotation CAREFUL the operator | works only in python 3.9
+
+        if "category" in new_file_dict : #empty IS a category
             if location in new_dict :
                 new_dict[location].append(new_file_dict)
             else :
