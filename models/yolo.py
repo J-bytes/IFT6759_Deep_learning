f7d254dbd98aec6809cc27522991ba09e576714c
Seeeeeyo <selimgilon@gmail.com>
2022-03-24 18:20:28 -0400

    plan

--- models/yolo.py
+++ models/yolo.py
@@ -1,4 +1,5 @@
 """
+DAY 1:
 !git clone https://github.com/ultralytics/yolov5  # clone
 %cd yolov5
 on veut être dans models/yolov5
@@ -10,4 +11,15 @@ train: on veut jouer avec le batch size et avoir le maximum qui ne plante pas (a
 !python train.py --img 600 --batch 80 --epochs 50 --patience 5 --data data.yaml --weights yolov5l.pt --cache
 on peut juste essayer avec une seule epoch et voir qu'il y aura un log de l'experience qui est créé dans
 models/yolov5/runs/train/exp/
+"""
+
+
+"""
+DAY 2:
+Run 320 or 608 depending on the outcome of the 608 (see tonight) on a bigger model (medium? large?). ADD WANDB before
+"""
+
+"""
+DAY 3:
+RESULTS + faster rcnn etc
 """
