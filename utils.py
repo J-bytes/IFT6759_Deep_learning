fa47ffd1b769c43a7f373ffe725f210ef5a586bd
jonathan <joe199827@gmail.com>
2022-03-31 19:02:25 -0400

    changed image size to 320

--- utils.py
+++ utils.py
@@ -27,8 +27,8 @@ def set_parameter_requires_grad(model, feature_extract):
 
 
 preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
+    transforms.Resize(320),
+    transforms.CenterCrop(320),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
